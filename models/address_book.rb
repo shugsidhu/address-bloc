22334171f92dceb28754edae1973c712ca58fc1e
Shug <shugsidhu1108@gmail.com>
2018-05-17 21:48:56 -0700

    checkpoint csv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,39 +1,32 @@
 require_relative 'entry'
-require_relative 'controllers/menu_controller'
+require 'csv'
 
- menu = MenuController.new
+class AddressBook
+    attr_reader :entries
 
- system "clear"
- puts "Welcome to AddressBloc!"
-
- menu.main_menu
-
- class AddressBook
-   attr_reader :entries
-   def initialize
-      @entries = []
+    def initialize
+        @entries = []
     end
 
- def remove_entry(name, phone, email)
-   delete_entry = nil
+    def add_entry(name, phone_number, email)
 
-   @entries.each do |entry|
-     if name == entry.name && phone == entry.phone_number && email == entry.email
-       delete_entry = entry
-  end
- end
+        index = 0
+        entries.each do |entry|
+            if name < entry.name
+                break
+            end
+            index+=1
+        end
 
-   @entries.delete(delete_entry)
- end
+        entries.insert(index, Entry.new(name, phone_number, email))
+    end
 
- def add_entry(name, phone_number, email)
-   index = 0
-   entries.each do |entry|
-     if name < entry.name
-       break
-     end
-     index+= 1
-   end
-   entries.insert(index, Entry.new(name, phone_number, email))
- end
- end
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+         end
+    end
+end
